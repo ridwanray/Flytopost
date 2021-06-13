503d5f2ad2b727f2243987059883e639a96ac505
Samson <samsonugwuodo@gmail.com>
2021-06-13 00:31:49 -0400

    Updated database model

--- flyappbackend/base/settings.py
+++ flyappbackend/base/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '$k7+$9*8_y^-u-%e!(c(8$0m4n9_x0h(+qfe)3a)rvmo8+t)w4'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
